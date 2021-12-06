35bbf6dcb2f7c2fea9c44bbba317d5dc0d8a03eb
Ross Singer <rxs@talis.com>
2021-12-06 17:28:06 -0500

    Add OIDC_Login tests

--- src/lti/LTI_OIDC_Login.php
+++ src/lti/LTI_OIDC_Login.php
@@ -11,10 +11,10 @@ class LTI_OIDC_Login {
      * Constructor
      *
      * @param Database  $database   Instance of the database interface used for looking up registrations and deployments.
-     * @param FileCache $cache      Instance of the Cache interface used to loading and storing launches. If non is provided launch data will be store in $_SESSION.
+     * @param Cache     $cache      Instance of the Cache interface used to loading and storing launches. If non is provided launch data will be store in $_SESSION.
      * @param Cookie    $cookie     Instance of the Cookie interface used to set and read cookies. Will default to using $_COOKIE and setcookie.
      */
-    function __construct(Database $database, FileCache $cache = null, Cookie $cookie = null) {
+    function __construct(Database $database, Cache $cache = null, Cookie $cookie = null) {
         $this->db = $database;
         if ($cache === null) {
             $cache = new FileCache();
@@ -31,12 +31,12 @@ class LTI_OIDC_Login {
      * Static function to allow for method chaining without having to assign to a variable first.
      *
      * @param Database  $database   Instance of the database interface used for looking up registrations and deployments.
-     * @param FileCache $cache      Instance of the Cache interface used to loading and storing launches. If non is provided launch data will be store in $_SESSION.
+     * @param Cache     $cache      Instance of the Cache interface used to loading and storing launches. If non is provided launch data will be store in $_SESSION.
      * @param Cookie    $cookie     Instance of the Cookie interface used to set and read cookies. Will default to using $_COOKIE and setcookie.
      * 
      * @return self
      */
-    public static function newInstance(Database $database, FileCache $cache = null, Cookie $cookie = null) {
+    public static function newInstance(Database $database, Cache $cache = null, Cookie $cookie = null) {
         return new LTI_OIDC_Login($database, $cache, $cookie);
     }
 
@@ -112,8 +112,10 @@ class LTI_OIDC_Login {
             throw new OIDC_Exception("Could not find login hint", 1);
         }
 
+        $clientId = isset($request['aud']) ? $request['aud'] : null;
+
         // Fetch Registration Details.
-        $registration = $this->db->find_registration_by_issuer($request['iss']);
+        $registration = $this->db->find_registration_by_issuer($request['iss'], $clientId);
 
         // Check we got something.
         if (empty($registration)) {
---
+++ tests/unit/LTI_OIDC_Login_Test.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace IMSGlobal\LTI\Tests\unit;
+
+use IMSGlobal\LTI\LTI_OIDC_Login;
+
+class LTI_OIDC_Login_Test extends TestBase {
+    private $launchUrl = 'https://example.com/lti1p3/launch';
+
+    public function testNewInstance()
+    {
+        $this->assertInstanceOf('IMSGlobal\LTI\LTI_OIDC_Login', LTI_OIDC_Login::newInstance(
+            new DummyDatabase()
+        ));
+    }  
+    
+    public function testDoOidcLoginRedirectEmptyLaunchUrl()
+    {
+        $this->setExpectedException('IMSGlobal\LTI\OIDC_Exception', 'No launch URL configured');
+
+        LTI_OIDC_Login::newInstance(new DummyDatabase())->do_oidc_login_redirect('');
+    }
+
+    public function testValidateOidcLoginNoIssuer()
+    {
+        $this->setExpectedException('IMSGlobal\LTI\OIDC_Exception', 'Could not find issuer');
+        LTI_OIDC_Login::newInstance(new DummyDatabase())->do_oidc_login_redirect(
+            $this->launchUrl,
+            []
+        );        
+    }
+
+    public function testValidateOidcLoginNoPasswordHint()
+    {
+        $this->setExpectedException('IMSGlobal\LTI\OIDC_Exception', 'Could not find login hint');
+        LTI_OIDC_Login::newInstance(new DummyDatabase())->do_oidc_login_redirect(
+            $this->launchUrl,
+            ['iss' => 'aaa']
+        );        
+    }   
+    
+    public function testValidateOidcLoginRegistrationNotFound()
+    {
+        $this->setExpectedException('IMSGlobal\LTI\OIDC_Exception', 'Could not find registration details');
+
+        /** @var DummyDatabase|\PHPUnit_Framework_MockObject_MockObject */
+        $registrationDatabase = $this->getMockBuilder('\IMSGlobal\LTI\Tests\unit\DummyDatabase')
+            ->setMethods(['find_registration_by_issuer'])
+            ->getMock();
+        
+        $registrationDatabase->expects($this->atLeastOnce())
+            ->method('find_registration_by_issuer')
+            ->with('xyz', null)
+            ->willReturn(null);
+
+        LTI_OIDC_Login::newInstance($registrationDatabase)->do_oidc_login_redirect(
+            $this->launchUrl,
+            [
+                'iss' => 'xyz',
+                'login_hint' => 'password123'
+            ]
+        );        
+    }  
+    
+    public function testDoOidcLoginRedirect()
+    {
+        /** @var \IMSGlobal\LTI\Tests\unit\helpers\InMemoryCache|\PHPUnit_Framework_MockObject_MockObject */
+        $cache = $this->getMockBuilder('\IMSGlobal\LTI\Tests\unit\helpers\InMemoryCache')
+            ->setMethods(['cache_nonce'])
+            ->getMock();
+        
+        $cache->expects($this->once())->method('cache_nonce')->with(
+            $this->stringStartsWith('nonce-')
+        );
+        
+        /** @var \IMSGlobal\LTI\Cookie|\PHPUnit_Framework_MockObject_MockObject $cookie */
+        $cookie = $this->getMockBuilder('\IMSGlobal\LTI\Cookie')
+            ->setMethods(['set_cookie'])
+            ->getMock();
+        
+        $cookie->expects($this->once())->method('set_cookie')->with(
+            $this->stringStartsWith('lti1p3_state-'),
+            $this->stringStartsWith('state-')
+        )->willReturn($cookie);
+
+        $redirect = LTI_OIDC_Login::newInstance(
+            new DummyDatabase(),
+            $cache,
+            $cookie
+        )->do_oidc_login_redirect(
+            $this->launchUrl,
+            [
+                'iss' => 'aaa',
+                'login_hint' => 'password123',
+                'lti_message_hint' => 'my message hint'
+            ]
+        );   
+        
+        $this->assertInstanceOf('\IMSGlobal\LTI\Redirect', $redirect);
+
+        $redirectUrl = parse_url($redirect->get_redirect_url());
+
+        $this->assertEquals('https', $redirectUrl['scheme']);
+        $this->assertEquals('example.com', $redirectUrl['host']);   
+        $this->assertEquals('/lti1p3/aaa/12345/auth_login_url', $redirectUrl['path']);
+        parse_str($redirectUrl['query'], $query);
+        $this->assertEquals('openid', $query['scope']);
+        $this->assertEquals('id_token', $query['response_type']);
+        $this->assertEquals('form_post', $query['response_mode']);
+        $this->assertEquals('none', $query['prompt']);
+        $this->assertEquals('12345', $query['client_id']);
+        $this->assertEquals($this->launchUrl, $query['redirect_uri']);
+        $this->assertStringStartsWith('state-', $query['state']);
+        $this->assertStringStartsWith('nonce-', $query['nonce']);
+        $this->assertEquals('password123', $query['login_hint']);
+        $this->assertEquals('my message hint', $query['lti_message_hint']);
+    }
+}
