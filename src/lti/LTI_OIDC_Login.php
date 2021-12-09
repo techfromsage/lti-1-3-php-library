9f691e39c42c1fe13907cfbaa7159806682dce6f
Ross Singer <rossfsinger@gmail.com>
2021-12-09 06:03:14 -0500

    Can't be a string

--- src/lti/LTI_OIDC_Login.php
+++ src/lti/LTI_OIDC_Login.php
@@ -43,8 +43,8 @@ class LTI_OIDC_Login {
     /**
      * Calculate the redirect location to return to based on an OIDC third party initiated login request.
      *
-     * @param string       $launch_url URL to redirect back to after the OIDC login. This URL must match exactly a URL white listed in the platform.
-     * @param array|string $request    An array of request parameters. If not set will default to $_REQUEST.
+     * @param string     $launch_url URL to redirect back to after the OIDC login. This URL must match exactly a URL white listed in the platform.
+     * @param array|null $request    An array of request parameters. If not set will default to $_REQUEST.
      *
      * @return Redirect Returns a redirect object containing the fully formed OIDC login URL.
      */
